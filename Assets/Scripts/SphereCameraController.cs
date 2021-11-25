c4fae880a1ee7a0631cc176a7e7b655701d72034
huangjiaying96 <jiaying.huang@plus.ai>
2021-11-25 20:04:36 +0800

    add SphereCameraController script

---
+++ Assets/Scripts/SphereCameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+// Spin and view the sphere with right drag. Zoom in or out with wheel.
+
+public class SphereCameraController : MonoBehaviour {
+    public float dragSensitivity = 1.0f; 
+    public float scrollSensitivity = 1.0f; 
+    public float zoomSize = 2.0f;
+    
+    private Vector3 cameraPosition;
+    
+    void Update () {
+        HandleDrag();        
+        HandleScroll();
+    }
+    
+    // Rotate sphere by dragging right mousebutton
+    private void HandleDrag() {
+        if (Input.GetMouseButtonDown(1)) {
+            cameraPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(1)) {
+            Vector3 moveDir = Input.mousePosition - cameraPosition;
+            Quaternion rotationX = Quaternion.AngleAxis(moveDir.x * dragSensitivity, transform.up);
+            Quaternion rotationY = Quaternion.AngleAxis(moveDir.y * dragSensitivity, -transform.right);
+            transform.localRotation = rotationX * rotationY * transform.localRotation;
+            cameraPosition = Input.mousePosition;
+        }
+    }
+    
+    // Zoom in/out by mouse scrollwheel
+    private void HandleScroll() {
+        zoomSize -= scrollSensitivity * CrossPlatformInputManager.GetAxis("Mouse ScrollWheel");
+        transform.position = transform.forward * -zoomSize;
+    }
+}
